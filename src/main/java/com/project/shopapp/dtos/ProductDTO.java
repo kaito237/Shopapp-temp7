655cd72cab1049bfdeea971de583b48fa5980df4
Trang <kaito2372000@gmail.com>
2024-08-25 11:43:59 +0900

    shopapp- temp1

--- src/main/java/com/project/shopapp/dtos/ProductDTO.java
+++ src/main/java/com/project/shopapp/dtos/ProductDTO.java
@@ -1,26 +1,33 @@
 package com.project.shopapp.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
 import lombok.*;
+import jakarta.validation.constraints.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@Data
+import java.util.List;
+
+@Data//toString
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class ProductDTO {
-    @Size(min=3, max =200, message = "title must be between 3 and 200 characters")
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 200, message = "Title must be between 3 and 200 characters")
     private String name;
-    @Min(value= 0, message = "price must be greater than or equal to O")
-    @Max(value = 1000, message = "price must be less than or equal to 10,000,000")
-    private  Float price;
-    private  String thumbnail;
+
+    @Min(value = 0, message = "Price must be greater than or equal to 0")
+    @Max(value = 10000000, message = "Price must be less than or equal to 10,000,000")
+    private Float price;
+
+    private String thumbnail;
+
     private String description;
-    @JsonProperty
-    private  String categoryId;
+
+    @JsonProperty("category_id")
+    private String categoryId;
+
+    private List<MultipartFile> files;
 
 }
